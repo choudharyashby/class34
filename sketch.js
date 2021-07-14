cc0793e8efc3fc74d92f3b56c4ac306c07149b03
choudharyashby <75932895+choudharyashby@users.noreply.github.com>
2021-07-14 20:52:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+
+
+
+function setup(){
+    var canvas = createCanvas(3000,800);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,600,1200,20);
+    
+
+    box1 = new Box(900,100,70,70);
+    box2 = new Box(900,100,70,70);
+    box3 = new Box(900,100,70,70);
+    box4 = new Box(900,100,70,70);
+    box5 = new Box(900,100,70,70);
+    box6 = new Box(900,100,70,70);
+    box7 = new Box(800,100,70,70);
+    box8 = new Box(800,100,70,70);
+    box9 = new Box(800,100,70,70);
+    box10 = new Box(800,100,70,70);
+    box11 = new Box(800,100,70,70);
+    box12 = new Box(800,100,70,70);
+    box13 = new Box(700,100,70,70);
+    box14 = new Box(700,100,70,70);
+    box15 = new Box(700,100,70,70);
+    
+   // bird = new Bird(200,50);
+   ball = new Ball(200,200,80,80)
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(ball.body,{x:500, y:50});
+}
+
+function draw(){
+background(0);
+    
+       
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+    box13.display();
+    box14.display();
+    box15.display();
+
+   
+   
+   
+   ball.display();
+    //bird.display();
+    //platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(ball.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
